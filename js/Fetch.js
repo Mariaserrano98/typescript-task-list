91960232b829ae9078d9dc54d871ec8cce1ce82e
maria <serranoortizmaria020@gmail.com>
2023-05-22 12:44:44 +0200

    comentarios al archivo fetch y repasar en casa del tasklist

--- js/Fetch.js
+++ js/Fetch.js
@@ -1,57 +1,57 @@
-export class Fetch {
-  BASE_URL = "http://localhost:8000/tasks";
+export class Fetch {  // Clase Fetch con métodos estáticos para realizar las peticiones al servidor
+  BASE_URL = "http://localhost:8000/tasks"; // URL base del servidor
   static async getAll() {
-    const response = await fetch("http://localhost:8000/tasks");
-    if (!response.ok) {
+    const response = await fetch("http://localhost:8000/tasks"); // fetch devuelve una promesa que se resuelve cuando se obtiene la respuesta del servidor (response)
+    if (!response.ok) {                                    // Si la respuesta no es correcta, se lanza un error con el mensaje de error correspondiente (statusText) y el código de error (status) 
       throw new Error(
-        `Error al obtener las tareas: ${response.status} ${response.statusText}`
+        `Error al obtener las tareas: ${response.status} ${response.statusText}` // statusText: mensaje de error del servidor
       );
     }
-    const data = await response.json();
-    return data;
+    const data = await response.json(); // Si la respuesta es correcta, se obtiene la respuesta en formato JSON y se devuelve (data) 
+    return data;                       // Se devuelve la respuesta en formato JSON
   }
 
-  static async create(task) {
-    const response = await fetch(this.BASE_URL, {
-      method: "POST",
-      headers: {
+  static async create(task) {  // Método para crear una tarea 
+    const response = await fetch(this.BASE_URL, { // Se realiza una petición POST a la URL base del servidor (BASE_URL) con el objeto task en formato JSON en el cuerpo de la petición (body) 
+      method: "POST",                            // El método de la petición es POST
+      headers: {                                // Se especifica el tipo de contenido del cuerpo de la petición (Content-Type) 
+        "Content-Type": "application/json",  // El tipo de contenido es JSON 
+      },
+      body: JSON.stringify(task),          // El cuerpo de la petición es el objeto task en formato JSON 
+    });
+    if (!response.ok) {                   // Si la respuesta no es correcta, se lanza un error con el mensaje de error correspondiente (statusText) y el código de error (status) 
+      throw new Error(                  // statusText: mensaje de error del servidor 
+        `Error al crear la tarea: ${response.status} ${response.statusText}` // status: código de error del servidor 
+      );
+    }
+    const data = await response.json(); // Si la respuesta es correcta, se obtiene la respuesta en formato JSON y se devuelve (data) 
+    return data;                      // Se devuelve la respuesta en formato JSON
+  }
+
+  static async update(task) { // Método para actualizar una tarea 
+    const response = await fetch(`${this.BASE_URL}${task.id}`, { // Se realiza una petición PATCH a la URL base del servidor (BASE_URL) con el id de la tarea a actualizar en la URL y el objeto task en formato JSON en el cuerpo de la petición (body)
+      method: "PATCH",                                           // El método de la petición es PATCH
+      headers: {                                             // Se especifica el tipo de contenido del cuerpo de la petición (Content-Type) 
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(task),
+      body: JSON.stringify(task), // El cuerpo de la petición es el objeto task en formato JSON 
     });
-    if (!response.ok) {
-      throw new Error(
-        `Error al crear la tarea: ${response.status} ${response.statusText}`
+    if (!response.ok) {       // Si la respuesta no es correcta, se lanza un error con el mensaje de error correspondiente (statusText) y el código de error (status) 
+      throw new Error(    // statusText: mensaje de error del servidor 
+        `Error al actualizar la tarea: ${response.status} ${response.statusText}` // status: código de error del servidor 
       );
     }
-    const data = await response.json();
-    return data;
+    const data = await response.json(); // Si la respuesta es correcta, se obtiene la respuesta en formato JSON y se devuelve (data) 
+    return data;                     // Se devuelve la respuesta en formato JSON
   }
 
-  static async update(task) {
-    const response = await fetch(`${this.BASE_URL}${task.id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(task),
+  static async delete(id) { // Método para eliminar una tarea 
+    const response = await fetch(`${this.BASE_URL}${id}`, { // Se realiza una petición DELETE a la URL base del servidor (BASE_URL) con el id de la tarea a eliminar en la URL 
+      method: "DELETE", // El método de la petición es DELETE 
     });
-    if (!response.ok) {
-      throw new Error(
-        `Error al actualizar la tarea: ${response.status} ${response.statusText}`
-      );
-    }
-    const data = await response.json();
-    return data;
-  }
-
-  static async delete(id) {
-    const response = await fetch(`${this.BASE_URL}${id}`, {
-      method: "DELETE",
-    });
-    if (!response.ok) {
-      throw new Error(
-        `Error al eliminar la tarea: ${response.status} ${response.statusText}`
+    if (!response.ok) {  // Si la respuesta no es correcta, se lanza un error con el mensaje de error correspondiente (statusText) y el código de error (status) 
+      throw new Error( // statusText: mensaje de error del servidor 
+        `Error al eliminar la tarea: ${response.status} ${response.statusText}` // status: código de error del servidor 
       );
     }
   }
